8ed07aca31886f16dffa2aaa397ae3703313bba4
kimhaun <wha02068@gmail.com>
2020-11-01 14:38:26 +0900

    한 줄의 데이터 실반영을 위한 준비작업  - findViewById, mList에서 데이터 가져오기

--- app/src/main/java/com/appisna/blogspot/jickbangcopy_20201101/adapters/RoomAdapter.kt
+++ app/src/main/java/com/appisna/blogspot/jickbangcopy_20201101/adapters/RoomAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.appisna.blogspot.jickbangcopy_20201101.R
 import com.appisna.blogspot.jickbangcopy_20201101.datas.Room
 import kotlinx.android.synthetic.main.activity_main.view.*
+import org.w3c.dom.Text
 
 class RoomAdapter(
     val mContext:Context,
@@ -24,6 +26,15 @@ class RoomAdapter(
             tempRow = inf.inflate(R.layout.room_list_item, null)
         }
         val row = tempRow!!
+
+        val roomData = mList[position]
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloor = row.findViewById<TextView>(R.id.addressAndFloor)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+
+
+
 
         return row
     }
