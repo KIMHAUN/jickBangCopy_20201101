de0a949b855206cc081730f43b90d4edd157b7fa
kimhaun <wha02068@gmail.com>
2020-11-01 14:05:30 +0900

    방 관련 목록을 뿌려주는adapter의 뼈대작업

---
+++ app/src/main/java/com/appisna/blogspot/jickbangcopy_20201101/adapters/RoomAdapter.kt
@@ -0,0 +1,32 @@
+package com.appisna.blogspot.jickbangcopy_20201101.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.appisna.blogspot.jickbangcopy_20201101.R
+import com.appisna.blogspot.jickbangcopy_20201101.datas.Room
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class RoomAdapter(
+    val mContext:Context,
+    val resId: Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList){
+
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+
+
+}
