ae3360fbc4ec67cd4f38f58dbbe7fb377a746161
kimhaun <wha02068@gmail.com>
2020-11-01 12:44:47 +0900

    리스트뷰 배치 + 방 정보 표현하는 클래스 생성

---
+++ app/src/main/java/com/appisna/blogspot/jickbangcopy_20201101/datas/Room.kt
@@ -0,0 +1,13 @@
+package com.appisna.blogspot.jickbangcopy_20201101.datas
+
+class Room(
+    val price:Int,
+    val address:String,
+    val floor : Int,
+    val description : String) {
+
+
+
+
+
+}
