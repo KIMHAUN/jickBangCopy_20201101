24fa8c5146558fbd98e337a015ad8d8a395af7c5
kimhaun <wha02068@gmail.com>
2020-11-14 11:49:55 +0900

    스플래쉬 화면만 테마 변경(어플 이름 안나오게 noActionBar thems.xml에 있는 것과 xxx.xxx.noActionBar통일시킴.

--- app/src/main/java/com/appisna/blogspot/jickbangcopy_20201101/SplashActivity.kt
+++ app/src/main/java/com/appisna/blogspot/jickbangcopy_20201101/SplashActivity.kt
@@ -8,6 +8,9 @@ import android.os.Looper
 
 class SplashActivity : BaseActivity() {
 
+    //onCreate: 액티비티가 메모리에 등록될 때(인스턴스화) 안드로이드 os가 실행함.
+    //(액티비티의 시작점으로 동작하는 함수)
+    //onCreate 내부 내용은 최소 1번은 반드시 실행된다고 보장됨.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -22,10 +25,11 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
         //로딩이 끝나면(2.5초 정도 후에) 메인 화면으로 이동 => 로딩화면 종료.
-
+        //핸들러 자동완성시 android에서 주는 걸로 자동완성.
         val myHandler = Handler(Looper.getMainLooper())
         myHandler.postDelayed({
             //myHander 에게 이 중괄호 내부의 일을 해달라고 요청(할일 등록post)
+            //실제로 시간이 지나고 실행될 내용.
             val myIntent = Intent(mContext, MainActivity::class.java)
             startActivity(myIntent)
             finish() //로딩화면 끄기
